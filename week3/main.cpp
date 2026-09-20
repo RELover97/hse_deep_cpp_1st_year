f5e7152d4caca79ed9e6d0c0a4e7c897b1475c16
RELover97 <re.forever.lover.97@gmail.com>
2026-09-20 18:26:15 +0300

    Wallet constructor

--- week3/main.cpp
+++ week3/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <exception>
 
 class Wallet {
     int id;
@@ -6,6 +7,14 @@ class Wallet {
     int limit;
 
 public:
+
+    explicit Wallet(int l)
+    : id(1), balance(0), limit(l)
+    {
+        if (l <= 0) {
+            throw std::invalid_argument("Limit must be positive");
+        }
+    }
 
     void deposit(int amount);
 
@@ -18,7 +27,10 @@ public:
 
 int main() 
 {
-    Wallet w;
+    // Wallet w1;
+
+    Wallet w(1000);
+    // Wallet w2 = 3000;
 
     return 0;
 }
